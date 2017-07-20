21ad26d837a9f407846aca2f4f79b31902ccabd7
simonepri <simone.pri@hotmail.it>
2017-07-20 17:25:11 +0200

    Fix typo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function errorExist(type) {
  * @param  {String} type The type of error to throw
  * @param  {String} msg An error message
  * @param  {(String|Number)} errno An unique error id code to let clients handle the error
- * @param  {...} [debug] An undefined number of contex-debug informations to pass
+ * @param  {...} [debug] An undefined number of contex-debug information to pass
  */
 function throwError(type, msg, errno, ...debug) {
 	debug = debug || [];
